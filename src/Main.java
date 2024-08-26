99c55a345790edefd16e112977d1b4b0ddb55608
Siddharth Sahu <siddharthsahu.180@gmail.com>
2024-08-26 23:41:07 +0530

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,116 @@
-public class Main
-{
-    public static void main(String[] args)
-        {
-            int num1 = 5;
-            int num2 = 3;
-            int result = num1+num2;
-            System.out.println(result);
-        }
+//public class Main
+//Video 6 Variables in Java
+//{
+//    public static void main(String[] args)
+//        {
+//            int num1 = 5;
+//            int num2 = 3;
+//            int result = num1+num2;
+//            System.out.println(result);
+//        }
+//}
+
+//public class Main
+//Video 7 Data types in Java
+//{
+//    public static void main(String[] args)
+//    {
+//        int i = 5;
+//        float f = 5.6f;
+//        byte by = 127;
+//        short sh = 255;
+//        long l = 2342l;
+//        double d = 2.5;
+//        char c = 'k';
+//        boolean b = true;
+//
+//    }
+//}
+
+//public class Main
+////Video 9 Type conversion in Java
+//{
+//    public static void main(String[] args)
+//    {
+//      byte b = 127;
+//      int a = 12;
+//      int a = 257;
+//      byte k = (byte) a;
+//      byte a = 30;
+//      byte b = 10;
+//      int result = a*b;
+//      System.out.println(result);
+//    }
+//}
+
+//public class Main
+//Video 10 Arithmetic Operators in Java
+//{
+//    public static void main(String[] args)
+//        {
+//       int num1 = 5;
+//       int num2 = 2;
+//       int result = num1 + num2;
+//       System.out.println(result);
+//        int num1 = 5;
+//        int num2 = 2;
+//        int result = num1*num2;
+//        System.out.println(result);
+//    }
+//        int num1 = 5;
+//        int num2 = 2;
+//        int result = num1 % num2;
+//        System.out.println(result);
+//    }
+
+
+//        num += 2;
+//        num -= 2;
+//        num *= 2;
+//        num ++;
+//        ++ num;
+//        System.out.println(num);
+//        }
+//   }
+
+//public class Main
+//{
+//Video 11 Relational Operators in Java
+
+//    public static void main (String[] args)
+//    {
+//        int x = 5;
+//        int y = 2;
+//        boolean result = x > y;
+//        System.out.println(result);
+
+//        int x = 6;
+//        int y = 6;
+//        boolean result = x >= y;
+//        System.out.println(result);
+
+//        int x = 7;
+//        int y = 5;
+//        boolean result = x != y;
+//        System.out.println(result);
+
+//        int x = 6;
+//        int y = 6;
+//        boolean result = x == y;
+//        System.out.println(result);
+//    }
+//}
+
+public class Main {
+//Video 12 Logical Operators in Java
+
+    public static void main(String[] args) {
+        int x = 7;
+        int y = 5;
+        int a = 5;
+        int b = 10;
+        boolean result = a > b;
+        System.out.println(!result);
+    }
+
 }
