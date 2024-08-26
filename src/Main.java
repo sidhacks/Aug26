928013d56f01ab0cac981a905f7fcc867c95433a
Siddharth Sahu <siddharthsahu.180@gmail.com>
2024-08-26 20:23:36 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args)
+        {
+            int num1 = 5;
+            int num2 = 3;
+            int result = num1+num2;
+            System.out.println(result);
+        }
+}
